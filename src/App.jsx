dd85431e1503d7321bd647788d0c3374760883f4
dauzkepow <dauzcool20@gmail.com>
2025-09-06 01:02:24 +0700

    state_useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -36,7 +36,8 @@ function App() {
 export default App
 */
 
-//1. JSX
+/*
+//2. JSX
 
 //menulis JSX dasar
 const name = "Daus Kepow";
@@ -54,3 +55,132 @@ export default App;
 
 
 //menyisipkan ekspresi Javascript dengan { }
+*/
+
+/*
+//3. Component
+
+//buat component Welcome
+function Welcome() {
+  return <h1>Halo, ini adalah component Welcome</h1>;
+}
+
+function Nama() {
+  return <h4>Nama Saya Daus</h4>
+}
+
+//import component dari file lain
+import Header from './components/Header';
+
+//Component Welcome digunakan dalam component App
+function App() {
+  return (
+    <div>
+      <Welcome />
+      <Welcome />
+      <Header />
+      <Nama />
+    </div>
+  );
+}
+
+export default App;
+*/
+
+/*
+//4. Props
+//mengirim props bernama name ke component Welcome
+function Welcome(props) {
+  return <h1>Halo, {props.name}</h1> //pakai {props.name}
+}
+
+function UserCard(props1) {
+  return (
+    <div>
+      <h2>{props1.name}</h2>
+      <p>Umur: {props1.age}</p>
+    </div>
+  );
+}
+
+//destructuring
+//agar tidak perlu menulis props.name berkali-kali
+function Data ({nama, umur, alamat, status}) {
+  return (
+    <>
+      <h4>{nama}</h4>
+      <h4>Umur: {umur}</h4>
+      <h4>{alamat}</h4>
+      <h4>{status}</h4>
+    </>
+    
+  );
+}
+
+function App() {
+  return (
+    //masukkan data props agar bisa ditampilkan ke Welcome()
+    <>
+      <Welcome name="Daus" />
+      <Welcome name="Ajeng" />
+      <Welcome name="Bisma" />
+      <UserCard name="Bismun" age={3} />
+      <Data nama="Satria" umur={5} alamat="Cemani" status="Anak"/>
+    
+      
+    </>
+  );
+}
+
+export default App;
+*/
+
+//5. state & useState
+//state = nilai awal
+//setState = mengubah nilai state
+//const [state, setState] = useState(initialValue);
+
+//contoh menampilkan teks dan mengubahnya saat tombol ditekan
+import { useState } from 'react';
+
+function App() {
+  //inisialisasi state
+  const [message, setMessage] = useState("Halo, React!");
+  const [count, setCount] = useState(0); //nilai awal 0
+
+  //state todos menyimpan array berisi string
+  const [todos, setTodos] = useState(["Belajar React", "Belajar JSX"]);
+  //fungsi addTodo menambahkan item baru ke dalam array menggunakan 
+  //spread operator (...)
+  const addTodo = () => {
+    setTodos([...todos, "Belajar State"]);
+  }
+
+  //daftar todo ditampilkan menggunakan .map()
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      <button onClick={() => setMessage("State telah berubah!")}>
+        Ubah Pesan
+      </button>
+
+      <br></br>
+      
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Tambah</button>
+      <button onClick={() => setCount(count - 1)}>Kurangi</button>
+
+      <br></br>
+      <h1>Belajar Todo:</h1>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      <button onClick={addTodo}>Tambah Todo</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+//#3 Components file terpisah
+
+function Header() {
+    return <h1>Ini adalah Header</h1>
+}
+
+export default Header;
